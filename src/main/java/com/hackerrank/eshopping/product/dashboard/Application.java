9a90b5b24aa086038e06cafbca358d06dc59433f
Paul Allen <pgallen@gmail.com>
2022-07-19 09:53:11 -0600

    Initial PG Native image
    Initial working PostgreSQL Native Image. Not fully tested.  No Docker JVM image support yet.

--- src/main/java/com/hackerrank/eshopping/product/dashboard/Application.java
+++ src/main/java/com/hackerrank/eshopping/product/dashboard/Application.java
@@ -3,10 +3,13 @@ package com.hackerrank.eshopping.product.dashboard;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.nativex.hint.TypeHint;
+import org.hibernate.dialect.PostgreSQL95Dialect;
 
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.hackerrank.eshopping")
+@TypeHint(types = PostgreSQL95Dialect.class, typeNames = "org.hibernate.dialect.PostgreSQLDialect")
 public class Application
 {
     public static void main (String[] args)
